ea44fabb87fafac690669f15589ea6fee352b71e
wynprtm <wayanpratama38@gmail.com>
2025-01-06 00:53:24 +0700

    Adding Comment on The Code

--- cses/introductoryproblem/repetitions.cpp
+++ cses/introductoryproblem/repetitions.cpp
@@ -2,23 +2,33 @@
 using namespace std;
 
 int main(){
+    // Initialization
     string dna;
-    cin>>dna;
     int count=0;
     int highestStreak=0;
+    
+    // Input string
+    cin>>dna;
+
+    // Checking the highest streak
     if(dna.size()==1){
         cout<<highestStreak+1<<endl;
     }else{
         for(int i=0;i<dna.size()-1;i++){
             if(dna[i]==dna[i+1]){
+                // Increment count
                 count++;
+
+                // Updating highest streak
                 if(count>highestStreak){
                     highestStreak=count;
                 }
+            // Reset count if the next character is different
             }else{
                 count=0;
             }     
         }
+        // Output the highest streak
         cout<<highestStreak+1<<endl;
     }   
 }
--- hackerrank/30DaysOfCode/day1.cpp
+++ hackerrank/30DaysOfCode/day1.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main(){
+    // Initialization
     string text;
+
+    // Input String
     getline(cin,text);
+
+    // Output result
     cout<<"Hello, World.\n";
     cout<<text<<endl;
     return 0;
