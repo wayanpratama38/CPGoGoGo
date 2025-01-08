87f2e566a8269e5ae618fb3c1e7773f027828e0d
wynprtm <wayanpratama38@gmail.com>
2025-01-08 23:34:05 +0700

    Trying to Solve Number Spiral in CSES

---
+++ cses/introductoryproblem/numberspiral.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        long long x,y;
+        cin>>y>>x;
+
+    }
+}
+
+// if odd n squared
+// if even n - 1 squared + 1
+
+// if second layer find the largest x,y
+// if odd tile largest squared - (layer - 1 )
+// if even tile largest squared + 1 ( layer - 1)
+
+// 3,3
+// 9 - (3 - 1 )
+// 9 - (2 )
+// 7 
+
+
+// 4,3
+// 16 - ( 3 - 1)
+// 14
+
+// 5,3
+// 25 - (5 - 1)
+
+// 3,5
+// 25 - (3 - 1) 
